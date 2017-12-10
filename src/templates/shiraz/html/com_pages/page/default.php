3a4663fb68a762fa81b57c12663783a9aadf5a96
wboston1 <william.boston@wsu.edu>
2017-12-10 00:08:24 -0800

    changed defautls, and added sparq colors and logo

--- src/templates/shiraz/html/com_pages/page/default.php
+++ src/templates/shiraz/html/com_pages/page/default.php
@@ -1,44 +1,32 @@
 <? defined('KOOWA') or die('Restricted access'); ?>
 
-<? @title('Anahita - Open source social networking platform and framework'); ?>
-<? @description('a remarkable open source platform for building knowledge sharing apps and services'); ?>
+<? @title('SparqEdu - Inspiration Powered by Connection'); ?>
+<? @description('educational, personal, & career support for online students'); ?>
 
 <div class="row">
 	<div class="span8 offset2">
 		<div class="hero-unit text-center">
-			<img src="https://s3.amazonaws.com/anahitapolis.com/media/logos/homepage_logo.png" alt="Anahita social networking platform and framework" />
+			<img src="src/templates/shiraz/css/style2/images/sparq_logo.png" alt="Inspiration Powered by Connection" /> 
 
 			<h1>Anahita</h1>
 
 			<p class="lead">open source social networking platform</p>
 
 			<p>
-				<a class="btn btn-primary" href="https://www.GetAnahita.com" target="_blank">
-					GetAnahita.com
+				<a class="btn btn-primary" href="https://http://www.sparqedu.com/" target="_blank">
+					sparqedu.com
 				</a>
 
 				<a title="Twitter" class="btn btn-twitter" href="https://www.twitter.com/anahitapolis" target="_blank">
 					<i class="icon-twitter"></i>
 				</a>
 
-				<a title="Facebook" class="btn btn-facebook" href="https://www.facebook.com/anahitasocial" target="_blank">
+				<a title="Facebook" class="btn btn-facebook" href="https://www.facebook.com/SPARQFORSTUDENTS/" target="_blank">
 					<i class="icon-facebook"></i>
-				</a>
-
-				<a title="Google+" class="btn btn-googleplus" href="https://plus.google.com/111222489368350494929/posts" target="_blank">
-					<i class="icon-googleplus"></i>
-				</a>
-
-				<a title="Blog" class="btn btn-feed" href="http://blog.getanahita.com" target="_blank">
-					<i class="icon-feed"></i>
-				</a>
-
-				<a title="GitHub" class="btn btn-github" href="https://github.com/anahitasocial/anahita" target="_blank">
-					<i class="icon-github"></i>
 				</a>
 			</p>
 
-			<p>Follow <strong>#Anahita</strong></p>
+			<p>Follow <strong>#Sparq</strong></p>
 		</div>
 	</div>
 </div>
