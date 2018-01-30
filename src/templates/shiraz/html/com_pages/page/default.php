cdf006e71c910cedceb056b7319f87f56bfca31a
William <williamboston123@hotmail.com>
2018-01-29 21:03:08 -0800

    add: updated logo

--- src/templates/shiraz/html/com_pages/page/default.php
+++ src/templates/shiraz/html/com_pages/page/default.php
@@ -6,7 +6,7 @@
 <div class="row">
 	<div class="span8 offset2">
 		<div class="hero-unit text-center">
-			<img src="https://s3-us-west-2.amazonaws.com/sparqedu.com/sparq_logo.png" alt="Inspiration Powered by Connection" /> 
+			<img src="https://s3-us-west-2.amazonaws.com/sparq2/Sparq/sparq_logo.png " alt="Inspiration Powered by Connection" /> 
 
 			
 
